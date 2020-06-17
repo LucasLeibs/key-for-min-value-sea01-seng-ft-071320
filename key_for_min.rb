50e562e1352d92e017f1b3c47c5a3b66e9ddd0c7
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-06-17 19:07:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,5 +8,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if smallest_value == nil || value < smallest_value
       smallest_value = value 
-  
+      smallest_key = key 
 end
+end 
+  smallest key 
+end 
